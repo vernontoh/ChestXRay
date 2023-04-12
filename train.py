681f02589261380b63660742c710d37cfb8dbf53
hungchiayu1 <hungchiayu1@gmail.com>
2023-04-12 14:31:10 +0800

    added train.py

---
+++ train.py
@@ -0,0 +1,24 @@
+from model import NaiveConvolutionNetwork
+from utils import load_dataset,train_model
+import os
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.backends.cudnn as cudnn
+import torchvision
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+from read_data import ChestXrayDataSet
+from sklearn.metrics import roc_auc_score
+from sklearn.metrics import f1_score,accuracy_score
+from tqdm import tqdm
+
+
+def main():
+    
+    train_dataloader,test_dataloader = load_dataset(batch_size=64)
+    model = NaiveConvolutionNetwork()
+    train_model(model,train_dataloader,test_dataloader,device='cuda:1',n_epochs=3)
+
+if __name__ == "__main__":
+    main()

f4e2fd9223208112f3d643d15ead7de746b758c9
alift99 <alifdaffa.main@gmail.com>
2023-04-16 12:04:04 +0800

    Modified DenseNet & ResNet architectures

--- models/densenet.py
+++ models/densenet.py
@@ -4,115 +4,100 @@ from torch import Tensor, nn
 import torch.nn.functional as F
 
 
-class _Transition(nn.Sequential):
+class Transition(nn.Sequential):
     def __init__(self, num_input_features, num_output_features):
-        super(_Transition, self).__init__()
-        self.add_module('norm', nn.BatchNorm2d(num_input_features))
-        self.add_module('relu', nn.ReLU(inplace=True))
-        self.add_module('conv', nn.Conv2d(num_input_features, num_output_features,
-                                          kernel_size=1, stride=1, bias=False))
-        self.add_module('pool', nn.AvgPool2d(kernel_size=2, stride=2))
+        super(Transition, self).__init__()
+        self.norm = nn.BatchNorm2d(num_input_features)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv = nn.Conv2d(num_input_features, num_output_features, kernel_size=1, stride=1, bias=False)
+        self.pool = nn.AvgPool2d(kernel_size=2, stride=2)
 
 
-class _DenseLayer(nn.Module):
+class DenseLayer(nn.Module):
     def __init__(self, num_input_features, growth_rate, bn_size, drop_rate, memory_efficient=False):
-        super(_DenseLayer, self).__init__()
-        self.add_module('norm1', nn.BatchNorm2d(num_input_features)),
-        self.add_module('relu1', nn.ReLU(inplace=True)),
-        self.add_module('conv1', nn.Conv2d(num_input_features, bn_size *
-                                           growth_rate, kernel_size=1, stride=1,
-                                           bias=False)),
-        self.add_module('norm2', nn.BatchNorm2d(bn_size * growth_rate)),
-        self.add_module('relu2', nn.ReLU(inplace=True)),
-        self.add_module('conv2', nn.Conv2d(bn_size * growth_rate, growth_rate,
-                                           kernel_size=3, stride=1, padding=1,
-                                           bias=False)),
-        self.drop_rate = float(drop_rate)
+        super(DenseLayer, self).__init__()
+        self.relu = nn.ReLU(inplace=True)
+        self.norm1 = nn.BatchNorm2d(num_input_features)
+        self.conv1 = nn.Conv2d(num_input_features, bn_size * growth_rate, kernel_size=1, stride=1, bias=False)
+        self.norm2 = nn.BatchNorm2d(bn_size * growth_rate)
+        self.conv2 = nn.Conv2d(bn_size * growth_rate, growth_rate, kernel_size=3, stride=1, padding=1, bias=False)
+        self.dropout = nn.Dropout(drop_rate)
         self.memory_efficient = memory_efficient
 
-    def bn_function(self, inputs):
-        concated_features = torch.cat(inputs, 1)
-        bottleneck_output = self.conv1(self.relu1(self.norm1(concated_features)))  # noqa: T484
-        return bottleneck_output
-
-    def forward(self, input):  # noqa: F811
+    def forward(self, input):
         if isinstance(input, Tensor):
             prev_features = [input]
         else:
             prev_features = input
 
-        bottleneck_output = self.bn_function(prev_features)
-        new_features = self.conv2(self.relu2(self.norm2(bottleneck_output)))
-        if self.drop_rate > 0:
-            new_features = F.dropout(new_features, p=self.drop_rate,
-                                     training=self.training)
-        return new_features
+        concat_features = torch.cat(prev_features, 1)
+        bottleneck_output = self.conv1(self.relu(self.norm1(concat_features)))
+        next_features = self.conv2(self.relu(self.norm2(bottleneck_output)))
+        next_features = self.dropout(next_features)
+        return next_features
     
 
-class _DenseBlock(nn.ModuleDict):
-    def __init__(self, num_layers, num_input_features, bn_size, growth_rate, drop_rate, memory_efficient=False):
-        super(_DenseBlock, self).__init__()
+class DenseBlock(nn.Module):
+    def __init__(self, num_layers, num_input_features, bn_size, growth_rate, drop_rate=0, memory_efficient=False):
+        super(DenseBlock, self).__init__()
+        layers = []
         for i in range(num_layers):
-            layer = _DenseLayer(
+            layer = DenseLayer(
                 num_input_features + i * growth_rate,
                 growth_rate=growth_rate,
                 bn_size=bn_size,
                 drop_rate=drop_rate,
                 memory_efficient=memory_efficient,
             )
-            self.add_module('denselayer%d' % (i + 1), layer)
+            layers.append(layer)
+
+        self.dense_layers = nn.Sequential(*layers)
 
     def forward(self, init_features):
         features = [init_features]
-        for name, layer in self.items():
+        for layer in self.dense_layers:
             new_features = layer(features)
             features.append(new_features)
         return torch.cat(features, 1)
     
 
 class DenseNet(nn.Module):
-    def __init__(self, growth_rate=32, block_config=(6, 12, 24, 16),
-                 num_init_features=64, bn_size=4, drop_rate=0, num_classes=14, memory_efficient=False):
+    def __init__(self, growth_rate=24, block_config=(4, 8, 16, 12),
+                 n_init_features=48, bn_size=4, drop_rate=0, n_classes=14, memory_efficient=False):
         super(DenseNet, self).__init__()
 
-        # Convolution and pooling part from table-1
+        # First conv layer
         self.features = nn.Sequential(OrderedDict([
-            ('conv0', nn.Conv2d(3, num_init_features, kernel_size=7, stride=2,
+            ('conv0', nn.Conv2d(3, n_init_features, kernel_size=7, stride=2,
                                 padding=3, bias=False)),
-            ('norm0', nn.BatchNorm2d(num_init_features)),
+            ('norm0', nn.BatchNorm2d(n_init_features)),
             ('relu0', nn.ReLU(inplace=True)),
             ('pool0', nn.MaxPool2d(kernel_size=3, stride=2, padding=1)),
         ]))
-
-        # Add multiple denseblocks based on config 
-        # for densenet-121 config: [6,12,24,16]
-        num_features = num_init_features
-        for i, num_layers in enumerate(block_config):
-            block = _DenseBlock(
+        
+        n_features = n_init_features
+        for i, num_layers in enumerate(block_config):   # Add Dense blocks based on input parameters
+            block = DenseBlock(
                 num_layers=num_layers,
-                num_input_features=num_features,
+                num_input_features=n_features,
                 bn_size=bn_size,
                 growth_rate=growth_rate,
                 drop_rate=drop_rate,
                 memory_efficient=memory_efficient
             )
             self.features.add_module('denseblock%d' % (i + 1), block)
-            num_features = num_features + num_layers * growth_rate
-            if i != len(block_config) - 1:
-                # add transition layer between denseblocks to 
-                # downsample
-                trans = _Transition(num_input_features=num_features,
-                                    num_output_features=num_features // 2)
+            n_features = n_features + num_layers * growth_rate
+            if i != len(block_config) - 1:   # Add transition layer after each denseblock except last
+                trans = Transition(num_input_features=n_features,
+                                    num_output_features=n_features // 2)
                 self.features.add_module('transition%d' % (i + 1), trans)
-                num_features = num_features // 2
+                n_features = n_features // 2
 
-        # Final batch norm
-        self.features.add_module('norm5', nn.BatchNorm2d(num_features))
+        self.features.add_module('norm5', nn.BatchNorm2d(n_features))
 
-        # Linear layer
-        self.classifier = nn.Linear(num_features, num_classes)
+        self.classifier = nn.Linear(n_features, n_classes)
 
-        # Official init from torch repo.
+        # Use the official initialization method from PyTorch
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
                 nn.init.kaiming_normal_(m.weight)
@@ -124,7 +109,7 @@ class DenseNet(nn.Module):
 
     def forward(self, x):
         features = self.features(x)
-        out = F.relu(features, inplace=True)
+        out = nn.ReLU()(features)
         out = F.adaptive_avg_pool2d(out, (1, 1))
         out = torch.flatten(out, 1)
         out = self.classifier(out)
--- models/resnet.py
+++ models/resnet.py
@@ -19,55 +19,47 @@ class ResidualBlock(nn.Module):
         self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1)
         self.bn1 = nn.BatchNorm2d(out_channels)
         self.bn2 = nn.BatchNorm2d(out_channels)
+        self.relu = nn.ReLU()
 
     def forward(self, x):
         shortcut = self.shortcut(x)
-        x = nn.ReLU()(self.bn1(self.conv1(x)))
-        x = nn.ReLU()(self.bn2(self.conv2(x)))
+        x = self.relu(self.bn1(self.conv1(x)))
+        x = self.relu(self.bn2(self.conv2(x)))
         x = x + shortcut
         return nn.ReLU()(x)
     
 
-class ResNet18(nn.Module):
+class ResNet(nn.Module):
     def __init__(self, in_channels=3, outputs=14):
-        super(ResNet18, self).__init__()
+        super(ResNet, self).__init__()
         
         self.layer0 = nn.Sequential(
-            nn.Conv2d(in_channels, 64, kernel_size=7, stride=2, padding=3),
+            nn.Conv2d(in_channels, 32, kernel_size=5, stride=2, padding=2),
             nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
-            nn.BatchNorm2d(64),
+            nn.BatchNorm2d(32),
             nn.ReLU()
         )
 
         self.layer1 = nn.Sequential(
-            ResidualBlock(64, 64, downsample=False),
-            ResidualBlock(64, 64, downsample=False)
+            ResidualBlock(32, 32, downsample=False)
         )
 
         self.layer2 = nn.Sequential(
-            ResidualBlock(64, 128, downsample=True),
-            ResidualBlock(128, 128, downsample=False)
+            ResidualBlock(32, 64, downsample=True)
         )
 
         self.layer3 = nn.Sequential(
-            ResidualBlock(128, 256, downsample=True),
-            ResidualBlock(256, 256, downsample=False)
-        )
-
-        self.layer4 = nn.Sequential(
-            ResidualBlock(256, 512, downsample=True),
-            ResidualBlock(512, 512, downsample=False)
+            ResidualBlock(64, 128, downsample=True)
         )
 
         self.gap = nn.AdaptiveAvgPool2d(1)
-        self.fc = nn.Linear(512, outputs)
+        self.fc = nn.Linear(128, outputs)
 
     def forward(self, x):
         x = self.layer0(x)
         x = self.layer1(x)
         x = self.layer2(x)
         x = self.layer3(x)
-        x = self.layer4(x)
         x = self.gap(x)
         x = nn.Flatten()(x)
         x = self.fc(x)

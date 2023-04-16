413c60ecf916dc50568cba30a00d8175f5dfff8c
Vernon Toh <vernontoh@vernons-mbp-max.lan>
2023-04-16 17:46:37 +0800

    tidy some code

--- train_xvt.py
+++ train_xvt.py
@@ -1,24 +1,10 @@
 from models.naive_cnn import NaiveConvolutionNetwork
 from utils_xvt import load_dataset,train_model
-import os
-import numpy as np
-import torch
-import torch.nn as nn
-import torch.backends.cudnn as cudnn
-import torchvision
-import torchvision.transforms as transforms
-from torch.utils.data import DataLoader
-from read_data import ChestXrayDataSet
-from sklearn.metrics import roc_auc_score
-from sklearn.metrics import f1_score,accuracy_score
-from tqdm import tqdm
 
-import modeling_xvt
 import modeling_lightweight_xvt
 import modeling_lightweight_xvt_v2
 import configuration_xvt
 import importlib
-importlib.reload(modeling_xvt)
 importlib.reload(modeling_lightweight_xvt)
 importlib.reload(modeling_lightweight_xvt_v2)
 importlib.reload(configuration_xvt)
@@ -31,16 +17,23 @@ from configuration_xvt import XvtScheduler
 
 
 
-
-
 def main():
 
+    # indicating device to train with 
+    device = "mps"
+
+    # loading our training, validation and test set
     train_dataloader, val_dataloader, test_dataloader = load_dataset(batch_size=128)
+
+    # initializing configs for model and scheduler
     config = XvtConfig()
     args = XvtScheduler()
+    
+    # intialize model with config
     model = XvtForImageClassification(config)
 
-    train_model(model, train_dataloader, val_dataloader, test_dataloader, args, device='mps', use_weight_loss=True)
+    # start training model
+    train_model(model, train_dataloader, val_dataloader, test_dataloader, args, device=device, use_weight_loss=True)
 
 if __name__ == "__main__":
     main()
